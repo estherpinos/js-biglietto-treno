f1314563f4c9494d79c5472281609205aae198b6
estherpinos <estherpinos1@gmail.com>
2023-09-07 14:40:18 +0200

    Add [esercizio + js]

---
+++ script.js
@@ -0,0 +1,17 @@
+const kilometers = prompt('Quanti kilometri vuoi fare?');
+const age = prompt('Quanti anni hai?');
+let price = 0.21 * kilometers; 
+let discountChild = price * 0.2;
+let discountAdult = price * 0.4;
+
+
+if (age<18) price = price - discountChild;
+if (age>65) price = price - discountAdult;
+
+
+const message = `
+  
+  Il prezzo del biglietto è ${price.toFixed(2)}
+`;
+
+document.getElementById('output').innerHTML = message;
